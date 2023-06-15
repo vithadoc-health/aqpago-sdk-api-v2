e6d12a6249720bcd180083ee068dca78587b2640
Giovanne Oliveira <5747295+giovanne-oliveira@users.noreply.github.com>
2023-06-15 18:24:17 +0000

    fix: remove mixed return

--- src/SellerAqpago.php
+++ src/SellerAqpago.php
@@ -60,7 +60,7 @@ class SellerAqpago implements \JsonSerializable
     /**
      * @return mixed
      */
-    public function jsonSerialize():mixed
+    public function jsonSerialize()
     {
         return array_filter(
             get_object_vars($this)
